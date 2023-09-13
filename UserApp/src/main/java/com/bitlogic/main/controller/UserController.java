dce389c42e460a675f7e6746f5fe2cd4eaca22db
govindgond96 <princeganesh0596@gmail.com>
2023-09-13 15:04:10 +0530

    post api impl

--- UserApp/src/main/java/com/bitlogic/main/controller/UserController.java
+++ UserApp/src/main/java/com/bitlogic/main/controller/UserController.java
@@ -1,8 +1,18 @@
 package com.bitlogic.main.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bitlogic.main.model.User;
 
 @RestController
 public class UserController {
+	@PostMapping("/crateUser")
+	public User createUser(@RequestBody User user)
+	{
+		//iwill  write remainig code latter.
+		return user;
+	}
 
 }
