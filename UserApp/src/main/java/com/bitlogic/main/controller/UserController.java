8f93e940a01db5677fa9c0254a272d62bbcff592
kailashgaykwad <kailashgayakwad2999@gmail.com>
2023-09-13 02:47:14 -0700

    get api impl

--- UserApp/src/main/java/com/bitlogic/main/controller/UserController.java
+++ UserApp/src/main/java/com/bitlogic/main/controller/UserController.java
@@ -1,18 +1,30 @@
 package com.bitlogic.main.controller;
 
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bitlogic.main.model.User;
 
 @RestController
 public class UserController {
+
 	@PostMapping("/crateUser")
 	public User createUser(@RequestBody User user)
 	{
 		//iwill  write remainig code latter.
 		return user;
+	}
+	
+	@GetMapping("/expose_users")
+	public String viewUsers()
+	{
+		return "I will give userdata latter";
+
 	}
 
 }
